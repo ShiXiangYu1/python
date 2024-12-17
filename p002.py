40623bade3dea4c4ad4674626a36d5f8621680ee
‘shi_xiangyu’ <294802186@qq.com>
2024-12-17 12:27:00 +0800

    gengxin

--- p002.py
+++
@@ -1,9 +0,0 @@
-def get_jiecheng(number):
-    result = 1
-    while number > 0:
-        result *=number
-        number -= 1
-    return result
-print("jiecheng 6 =",get_jiecheng(6))
-print("jiecheng 3 =",get_jiecheng(3))
-print("jiecheng 100 =",get_jiecheng(100))
--- p026.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-unix_time = 1620747647
-
-datetime_obj = datetime.datetime.fromtimestamp(unix_time)
-datetime_str = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
-print(datetime_obj,type(datetime_obj))
-print(datetime_str,type(datetime_str))
